c369f0562ff8f832b91af72159093dc4bbe46950
Anna Steckel <annaextrasteckel@gmail.com>
2025-02-10 17:59:23 -0800

    schema, homepage, route middleware, applications controller, partials set up

---
+++ controllers/applications.js
@@ -0,0 +1,23 @@
+// DEPENDENCIES
+
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+// ROUTES
+
+// INDEX
+router.get('/', (req, res) => {
+    // 
+    
+    try {
+        res.render('applications/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// DEPENDENCIES
+
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -6,6 +8,10 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+const applicationsController = require('./controllers/applications.js');
 
 const authController = require('./controllers/auth.js');
 
@@ -14,36 +20,46 @@ const port = process.env.PORT ? process.env.PORT : '3000';
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+
+// MIDDLEWARE
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(passUserToView);
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+// ROUTES
+
+app.get('/', (req, res) => {
+    // Check if the user is signed in
+    if (req.session.user) {
+        // Redirect signed-in users to their applications index
+        res.redirect(`/users/${req.session.user._id}/applications`);
+    } else {
+        // Show the homepage for users who are not signed in
+        res.render('index.ejs');
+    }
 });
 
 app.use('/auth', authController);
 
+app.use(isSignedIn);
+
+app.use('/users/:userId/applications', applicationsController);
+
+// PORTS
+
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+    console.log(`The express app is ready on port ${port}!`);
 });
